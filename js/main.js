8dce1e36e7602c8b0b808199e6e0a3dfa74357b2
Ira <ilovebellsdaya@yandex.ru>
2020-05-19 10:25:27 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(function() {
+
+  // Fixed Header
+      let header = $("#header");
+      let intro = $("#aboutme");
+      let introH = intro.innerHeight();
+      let scrollPos = $(window).scrollTop();
+      let nav = $("#nav");
+      let navToggle = $("#navToggle");
+  
+      checkScroll(scrollPos, introH);
+  
+      $(window).on("scroll resize", function() {
+          introH = intro.innerHeight();
+          scrollPos = $(this).scrollTop();
+  
+          checkScroll(scrollPos, introH);
+      });
+      function checkScroll(scrollPos, introH) {
+  
+          if( scrollPos > introH ) {
+              header.addClass("fixed");
+          } else {
+              header.removeClass("fixed");
+          }
+      }
+  
+      // Scroll
+  
+          $("[data-scroll]").on("click", function(event) {
+              event.preventDefault();
+      
+              let elementId = $(this).data('scroll');
+              let elementOffset = $(elementId).offset().top;
+      
+              nav.removeClass("show");
+      
+              $("html, body").animate({
+                  scrollTop: elementOffset - 70
+              }, 700);
+          });
+      
+          // Nav
+          navToggle.on("click", function(event) {
+              event.preventDefault();
+      
+              nav.toggleClass("show");
+          });
+      
+        // CounterUp
+
+         $(".num").counterUp({
+    delay:10,time:1000
+ });
+ 
+          // Review
+         $(".review-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+ 
+});
+
+})(jQuery);
